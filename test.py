47b79686a26e7ef1f3bc32d20f52c85f2ece3bb2
Chris Cherniakov <taequn@gmail.com>
2023-07-31 20:05:05 +0300

    Removed

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# https://sns-api.bonfida.com/v2/arb/me-listings?categories=<comma separated categories>
-url = "https://sns-api.bonfida.com/v2/arb/me-listings?categories=9-club,99-club,english-nouns"
-r = requests.get(url)
-
-for x in r.json():
-    print(x)
