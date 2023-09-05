5d30b46dd2a0ffc2e506cb6e53961a2ccd4d287f
Chris Cherniakov <taequn@gmail.com>
2023-09-04 20:56:26 -0400

    Added docstring

--- cli.py
+++ cli.py
@@ -3,6 +3,15 @@ from utils.methods import init_message
 from utils.websocket import run_bot_cli
 from notifications.bot_init import init_tg_bot
 
+'''
+This is the main file of the project.
+
+Here we initialize the message that will be displayed when the bot is started.
+We also initialize if the bot will be started with the telegram bot or not.
+
+Finally, we connect to the websocket and start the bot.
+'''
+
 init_message()
 tg_bot_status = init_tg_bot()
 
